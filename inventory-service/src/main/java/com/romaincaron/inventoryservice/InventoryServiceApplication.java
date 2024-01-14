f83f3e083aad1a1d01a172a409e94eb9ebd0f4bb
romain caron <romain@awelty.com>
2024-01-14 13:11:42 +0100

    inventory service

--- inventory-service/src/main/java/com/romaincaron/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/romaincaron/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,11 @@
 package com.romaincaron.inventoryservice;
 
+import com.romaincaron.inventoryservice.model.Inventory;
+import com.romaincaron.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +14,19 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return (args) -> {
+			Inventory inventory = new Inventory();
+			inventory.setSkuCode("iphone_13");
+			inventory.setQuantity(100);
+
+			Inventory inventory2 = new Inventory();
+			inventory2.setSkuCode("iphone_13_red");
+			inventory2.setQuantity(0);
+
+			inventoryRepository.save(inventory);
+			inventoryRepository.save(inventory2);
+		};
+	}
 }
---
+++ inventory-service/src/main/java/com/romaincaron/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.romaincaron.inventoryservice.repository;
+
+import com.romaincaron.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
